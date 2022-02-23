92db9d452d0f12b0ea868452ff2577c6d5d76f27
Jacek <jacek.lajdecki@axonivy.com>
2022-02-23 16:41:03 +0100

    MARKET-223 Add first test

---
+++ msgraph-connector-test/src_test/com/axonivy/connector/office365/test/RestIvyTest.java
@@ -0,0 +1,34 @@
+package com.axonivy.connector.office365.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.microsoft.graph.MicrosoftGraphUser;
+
+import ch.ivyteam.ivy.environment.AppFixture;
+import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.environment.IvyTest;
+
+
+@IvyTest
+public class RestIvyTest{
+	
+  @BeforeEach
+  void beforeEach(AppFixture fixture) {
+    //Disable OAuth feature for mock rest service
+    fixture.config("RestClients.Microsoft 365 (Partial Graph API).Features", "ch.ivyteam.ivy.rest.client.mapper.JsonFeature");
+    fixture.config("RestClients.Microsoft 365 (Partial Graph API).Url", "{ivy.app.baseurl}/api/graphMock");
+    //fixture.var("Twitter-connector.Url", "{ivy.app.baseurl}/api/twitterMock");
+  }
+
+  @Test
+  public void restApi() {
+	  var response = Ivy.rest().client(UUID.fromString("007036dc-72d1-429f-88a7-ba5d5cf5ae58"))
+			  .path("/me").request().get().readEntity(MicrosoftGraphUser.class);
+	  assertThat(response.getMail()).isEqualTo("reguel.wermelinger@mailinator.com");
+  }
+}
