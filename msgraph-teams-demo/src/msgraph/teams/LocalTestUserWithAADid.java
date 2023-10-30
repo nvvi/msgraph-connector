2b40626f0f4312c02c5ce521d1ee0a622aa8b8f9
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2023-10-30 14:38:55 +0100

    refactor: use reflection, this internal method is no longer visible on LE 11.2

--- msgraph-teams-demo/src/msgraph/teams/LocalTestUserWithAADid.java
+++ msgraph-teams-demo/src/msgraph/teams/LocalTestUserWithAADid.java
@@ -1,14 +1,15 @@
 package msgraph.teams;
 
+import org.apache.commons.lang3.reflect.MethodUtils;
+
 import ch.ivyteam.ivy.security.ISecurityContext;
-import ch.ivyteam.ivy.security.internal.User;
 
 public class LocalTestUserWithAADid {
 
-  public static void assign(String username, String azureID) {
-    var usr = (User) ISecurityContext.current().users().find(username);
+  public static void assign(String username, String azureID) throws Exception {
+    var usr = ISecurityContext.current().users().find(username);
     if (usr != null && usr.getExternalId() == null) {
-      usr.setExternalId(azureID);
+      MethodUtils.invokeExactMethod(usr, "setExternalId", azureID);
     }
   }
 
