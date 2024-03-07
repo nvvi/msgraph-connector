233c463f571a523e4ef92ab19b017f078f72ec71
Khanh Nguyen <119989010+ndkhanh-axonivy@users.noreply.github.com>
2024-03-07 17:07:47 +0700

    Introduce new variable to set http client library (#91)
    * Introduce new variable to set http client library
    
    * Update RestOauthIvyTest with httpClientLibrary variable

--- msgraph-connector-test/src_test/com/axonivy/connector/office365/test/integration/RestOauthIvyTest.java
+++ msgraph-connector-test/src_test/com/axonivy/connector/office365/test/integration/RestOauthIvyTest.java
@@ -29,12 +29,14 @@ class RestOauthIvyTest{
     String appId = Ivy.var().get("microsoft-connector.appId");
     String secret = Ivy.var().get("microsoft-connector.secretKey");
     String tenantId = Ivy.var().get("microsoft-connector.tenantId");
+    String connectorProvider = Ivy.var().get("microsoft-connector.connectorProvider");
     var response = restClient.path("/applications")
       .property("AUTH.appId", appId)
       .property("AUTH.secretKey", secret)
       .property("AUTH.useAppPermissions", Boolean.TRUE.toString())
       .property("AUTH.scope", "https://graph.microsoft.com/.default")
       .property("AUTH.baseUri", "https://login.microsoftonline.com/" + tenantId + "/oauth2/v2.0")
+      .property("jersey.client.connectorProvider", connectorProvider)
       .request(MediaType.APPLICATION_JSON).get();
     assertThat(response.readEntity(String.class))
       .contains("\"uri\":\"http://localhost:8081/oauth2/callback\"")
